ae24d66538cb15669b95a6bc741bdbb8fdae66a9
G1d3l <129759849+G1d3l@users.noreply.github.com>
2024-11-04 18:41:23 -0300

    Update produto.py

--- app/model/produto.py
+++ app/model/produto.py
@@ -1,1 +1,9 @@
+from database.sessao import db
+
+class Produto(db.Model):
+    __tablename__ = 'produto'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(), nullable=False)
+    codigo = db.Column(db.String(), nullable=False)
+    categoria = db.Column(db.String(), nullable=False)
 
