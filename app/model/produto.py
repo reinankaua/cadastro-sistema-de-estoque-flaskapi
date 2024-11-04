8251c4110fce3daf7a31370bb6a3e5ac108aa700
G1d3l <129759849+G1d3l@users.noreply.github.com>
2024-11-04 20:09:08 -0300

    Update produto.py

--- app/model/produto.py
+++ app/model/produto.py
@@ -4,6 +4,6 @@ class Produto(db.Model):
     __tablename__ = 'produto'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(), nullable=False)
-    codigo = db.Column(db.String(), nullable=False)
-    categoria = db.Column(db.String(), nullable=False, unique=True)
+    codigo = db.Column(db.String(), nullable=False, unique=True)
+    categoria = db.Column(db.String(), nullable=False)
 
