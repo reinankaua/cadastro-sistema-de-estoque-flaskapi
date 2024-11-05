d6ac564e2add8e5f0fbfd259183f0371872fb629
G1d3l <129759849+G1d3l@users.noreply.github.com>
2024-11-05 17:59:14 -0300

    Update produto.py

--- app/routes/produto.py
+++ app/routes/produto.py
@@ -18,10 +18,10 @@ def register_routes_produto(app):
 
         if Produto_existente:
              return jsonify('erro': 'endereço já registrado para outro produto'}) , 409
-            novo_produto = Produto(
-                nome=nome,
-                codigo=codigo,
-                categoria=categoria,
+        novo_produto = Produto(
+            nome=nome,
+            codigo=codigo,
+            categoria=categoria,
         )
         try:
             db.session.add(novo_produto)
@@ -31,8 +31,34 @@ def register_routes_produto(app):
         except Integrityerror:
             db.session.rollback()
             return jsonify('erro': 'Erro de integridade ao cadastrar produto'}) , 500
- 
 
+@app.route('/atualizar/cliente/<int:id>', methods=['PUT'])
+def atualizar_produto(id):
+    data = Produto.get_json()
+
+    produto = Produto.query.get_or_404(id)
+    novo_nome = data.get('Nome', produto.nome)
+    novo_codigo = data.get('Código', produto.codigo)
+    novo_categoria = data.get('Categoria', produto.categoria)
+
+    produto_existente = Produto.query.filter_by(categoria=novo_categoria).first()
+
+    if produto_existente and produto_existente.id != produto.id:
+        return jsonify({'erro': 'categoria já registrada para outro cliente'}), 409
+
+    produto.nome = novo_nome
+    produto.codigo = novo_codigo
+    produto.categoria = novo_categoria
+
+    try:
+        db.session.commit()
+
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({"erro": "Erro ao atualizar produto}), 500
+
+ return jsonify({"mensagem": "Produto atuaalizado com sucesso."}), 200
+    
     @app.route('/listar/produto/<int:id>', methods=['GET'])
     def listar_produto_por_id(id):
         produto = Produto.query.get_or_404(id)
