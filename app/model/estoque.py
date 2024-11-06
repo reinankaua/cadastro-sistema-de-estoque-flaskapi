bfd280f32cb15865fe1f775aecca51d08754a001
dvluizsm <Luizbomfim8@gmail.com>
2024-11-06 20:15:15 -0300

    Correção do modelo de estoque

--- app/model/estoque.py
+++ app/model/estoque.py
@@ -3,6 +3,6 @@ from database.sessao import db
 class Estoque(db.Model):
     __tablename__ = 'estoque'
     id = db.Column(db.Integer, primary_key=True)
-    ID_Cliente = db.Column(db.String(), nullable=False)
-    ID_Produto = db.Column(db.String(), nullable=False)
-    Quantidade = db.Column(db.String(), nullable=False, unique=True)
+    id_cliente = db.Column(db.String(), nullable=False)
+    id_produto = db.Column(db.String(), nullable=False)
+    quantidade = db.Column(db.Integer, nullable=False)
